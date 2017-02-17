2129bc9e0bcd9300ae185ee29f6ca33e2e2f4fd3
Anne <antastic461@gmail.com>
2017-02-17 08:59:59 -0800

    setup inital files with registration

---
+++ src/SonOfCod/Controllers/RolesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
+using SonOfCod.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Authorization;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SonOfCod.Controllers
+{
+    public class RolesController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/SonOfCod/Models/Newsletter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SonOfCod.Models
+{   [Table("Newsletters")]
+    public class Newsletter
+    {
+        [Key]
+        public int NewsletterId { get; set; }
+        public string Email { get; set; }
+        public string BusinessType { get; set; } 
+        public string PurchaserName { get; set; }
+
+    }
+}
