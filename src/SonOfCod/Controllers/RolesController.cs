024fbd1dc3d1195d1681a76ff0b172f43ca3fe19
Anne <antastic461@gmail.com>
2017-02-17 09:50:53 -0800

    add user roles

--- src/SonOfCod/Controllers/RolesController.cs
+++ src/SonOfCod/Controllers/RolesController.cs
@@ -15,12 +15,150 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace SonOfCod.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class RolesController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public RolesController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, ApplicationDbContext db)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _db = db;
+        }
         // GET: /<controller>/
+        [AllowAnonymous]
         public IActionResult Index()
+        {
+            var roles = _db.Roles.ToList();
+            return View(roles);
+        }
+        [AllowAnonymous]
+        public IActionResult Create()
         {
             return View();
         }
+        [AllowAnonymous]
+        [HttpPost, ActionName("Create")]
+        public IActionResult CreateRole()
+        {
+            try
+            {
+                string roleName = HttpContext.Request.Form["RoleName"];
+                _db.Roles.Add(new IdentityRole()
+                {
+                    Name = roleName,
+                    NormalizedName = roleName.ToUpper()
+                });
+                _db.SaveChanges();
+                ViewBag.ResultMessage = "Role created successfully!";
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        [AllowAnonymous]
+        public IActionResult Delete(string RoleName)
+        {
+            var thisRole = _db.Roles.Where(r => r.Name.Equals(RoleName, StringComparison.CurrentCultureIgnoreCase)).FirstOrDefault();
+            _db.Roles.Remove(thisRole);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(string id)
+        {
+            var thisRole = _db.Roles.Where(r => r.Id == id).FirstOrDefault();
+            return View(thisRole);
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(IdentityRole role)
+        {
+            try
+            {
+                var thisRole = _db.Roles.Where(r => r.Id == role.Id).FirstOrDefault();
+                thisRole.Name = role.Name;
+                thisRole.NormalizedName = role.Name.ToUpper();
+                _db.Entry(thisRole).State = EntityState.Modified;
+                _db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        [AllowAnonymous]
+        public IActionResult Manage()
+        {
+            var list = _db.Roles.OrderBy(r => r.Name).ToList().Select(rr =>
+                new SelectListItem { Value = rr.Name.ToString(), Text = rr.Name }).ToList();
+            ViewBag.Roles = list;
+            return View();
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddRoleToUser(string userName, string roleName)
+        {
+            ApplicationUser user = _db.Users.Where(u => u.UserName.Equals(userName, StringComparison.CurrentCultureIgnoreCase)).FirstOrDefault();
+            await _userManager.AddToRoleAsync(user, roleName);
+
+            ViewBag.ResultMessage = "Role created successfully!";
+
+            var list = _db.Roles.OrderBy(r => r.Name).ToList().Select(rr =>
+                new SelectListItem { Value = rr.Name.ToString(), Text = rr.Name }).ToList();
+            ViewBag.Roles = list;
+            return View("Manage");
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> GetRoles(string UserName)
+        {
+            if (!string.IsNullOrWhiteSpace(UserName))
+            {
+                ApplicationUser user = _db.Users.Where(u => u.UserName.Equals(UserName, StringComparison.CurrentCultureIgnoreCase)).FirstOrDefault();
+
+                ViewBag.RolesForThisUser = await _userManager.GetRolesAsync(user);
+
+                var list = _db.Roles.OrderBy(r => r.Name).ToList().Select(rr => new SelectListItem { Value = rr.Name.ToString(), Text = rr.Name }).ToList();
+
+                ViewBag.Roles = list;
+
+            }
+            return View("Manage");
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteRoleForUser(string UserName, string RoleName)
+        {
+            ApplicationUser user = _db.Users.Where(u => u.UserName.Equals(UserName, StringComparison.CurrentCultureIgnoreCase)).FirstOrDefault();
+            bool inRole = await _userManager.IsInRoleAsync(user, RoleName);
+            if (inRole)
+            {
+                await _userManager.RemoveFromRoleAsync(user, RoleName);
+                ViewBag.ResultMessage = "Role removed from this user successfully!";
+            }
+            else
+            {
+                ViewBag.ResultMessage = "This user doesn't belong to selected role.";
+            }
+
+            var list = _db.Roles.OrderBy(r => r.Name).ToList().Select(rr => new SelectListItem { Value = rr.Name.ToString(), Text = rr.Name }).ToList();
+
+            ViewBag.Roles = list;
+
+            return View("Manage");
+        }
+
     }
 }
