9d571919a6a2b24f82fd97e5121cbc19c91c3b52
Anne <antastic461@gmail.com>
2017-02-17 15:11:58 -0800

    fix email list error

--- src/SonOfCod/Controllers/NewsletterController.cs
+++ src/SonOfCod/Controllers/NewsletterController.cs
@@ -26,7 +26,7 @@ namespace SonOfCod.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            return View(_db.Newsletters);
         }
 
         public IActionResult Create()
