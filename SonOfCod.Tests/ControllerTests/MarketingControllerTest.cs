0fb892af0b27223ba0aaafa005109deeee80b9b4
Anne <antastic461@gmail.com>
2017-02-17 14:16:00 -0800

    add test project and test for marketing controller

---
+++ SonOfCod.Tests/ControllerTests/MarketingControllerTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using SonOfCod.Controllers;
+using SonOfCod.Models;
+using Xunit;
+
+namespace SonOfCod.Tests.ControllerTests
+{
+    public class MarketingControllerTest
+    {
+        [Fact]
+        public void Get_ViewResult_Index_Test()
+        {
+            //Arrange
+            MarketingController controller = new MarketingController();
+
+            //Act
+            var result = controller.Index();
+
+            //Assert
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
