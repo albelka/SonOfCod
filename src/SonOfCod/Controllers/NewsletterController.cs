f7be2810e3b2bec2b3f6f25f91c88e28ad99b41f
Anne <antastic461@gmail.com>
2017-02-17 12:07:57 -0800

    begin newletter page

---
+++ src/SonOfCod/Controllers/NewsletterController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SonOfCod.Models;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SonOfCod.Controllers
+{
+    public class NewsletterController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Newsletter newsletter)
+        {
+
+            _db.Newsletters.Add(newsletter);
+            _db.SaveChanges();
+            return RedirectToAction("Index", "Account");
+        }
+    }
+
+   
+}
---
+++ src/SonOfCod/wwwroot/js/scripts.js
@@ -0,0 +1,6 @@
+﻿$(document).ready(function () {
+
+    $('.carousel').carousel({
+        intervak: 2000
+    });
+});
